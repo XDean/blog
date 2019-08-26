99b0f99f80184138e86bbd910b9629a1bece485b
Dean Xu <373216024@qq.com>
2019-08-26 13:56:09 +0800

    update config

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"os/exec"
 )
 
+var (
+	port = flag.Int("port", 11073, "Port")
+)
+
 func main() {
-	cmd := exec.Command("hugo", "server", "")
+	cmd := exec.Command("hugo", "server", "-p", string(*port), "--watch", "--disableLiveReload")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
