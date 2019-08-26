038d36f0e67295a2e86c46732f8c384cbb273ad1
Dean Xu <373216024@qq.com>
2019-08-26 18:07:01 +0800

    append port

--- main.go
+++ main.go
@@ -9,8 +9,10 @@ import (
 )
 
 var (
-	port    = flag.Int("port", 1313, "Port")
-	baseUrl = flag.String("baseUrl", "http://blog.xdean.com/", "Base URL")
+	port       = flag.Int("port", 1313, "Port")
+	baseUrl    = flag.String("baseUrl", "http://blog.xdean.com/", "Base URL")
+	appendPort = flag.Bool("appendPort", true, "Append port to base url")
+	extra      = flag.String("extra", "", "Extra hugo Param")
 )
 
 func main() {
@@ -19,7 +21,9 @@ func main() {
 	cmd := exec.Command("hugo", "server",
 		"-p", strconv.Itoa(*port),
 		"--watch", "--disableLiveReload",
-		"--baseURL", *baseUrl)
+		"--append-port", strconv.FormatBool(*appendPort),
+		"--baseURL", *baseUrl,
+		*extra)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
